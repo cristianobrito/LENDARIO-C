b815b39f1f8b1d2d9cb964efce90fbabd0dc288e
cristiano oliveira <britonano@gmail.com>
2023-03-23 11:48:02 -0300

    adicionados quarta

--- funcoes/main.c
+++ funcoes/main.c
@@ -9,6 +9,8 @@ void ler();                         /* ASSINATURA OU PROTOTIPO       */
 int main()
 {
   setlocale(LC_ALL, "Portuguese");  /* USANDO A ACENTUAÇÃO           */
+  time_t t;
+  time(&t);
   FILE *handle;
   handle=fopen("A1.txt","w");
   if(handle==NULL)
@@ -19,6 +21,7 @@ int main()
   fprintf(handle,"\tBEM VINDOS AO LISTÃO caça \n");
   fprintf(handle,"\t\twordlist:\n");
   fprintf(handle,"nano\ncristiano\njoyce\nakilles\ncarlos\nedson\n");
+  fprintf(handle,"\n \t%s", ctime(&t));
   fclose(handle);
   Sleep(500);
   ler();
@@ -27,17 +30,18 @@ int main()
 
 void ler()
 {
-    FILE *handle;
-    char buffer[1024];
-    handle=fopen("A1.txt","r");
-    if(handle==NULL)
+    FILE *handle;                      /* CRIA O HANDLE         */
+    char buffer[1024];                 /* PEGA A ENTRADA        */
+    handle=fopen("A1.txt","r");        /* RETORNA UM HANDLE     */
+    if(handle==NULL)                   /* TESTE LOGICO          */
     {
        puts("error");
-       exit(1);
+       exit(1);                        /* SAI SE DER ERRO       */
     }
-    while(fgets(buffer,1024,handle))
+    while(fgets(buffer,1024,handle))   /* LENDO O ARQUIVO       */
     {
-        printf("%s",buffer);
+        printf("%s",buffer);           /* IMPRIME NO CONSOLE    */
     }
-    fclose(handle);
+
+    fclose(handle);                    /* FECHA O ARQUIVO       */
 }
