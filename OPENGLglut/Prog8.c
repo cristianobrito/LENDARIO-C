f9b6c61ddb1343403dfe99d5c57b240e6819451f
cristiano oliveira <britonano@gmail.com>
2023-04-17 08:39:39 -0300

    segunda feira

---
+++ OPENGLglut/Prog8.c
@@ -0,0 +1,35 @@
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+void MyInit()
+{
+   glClearColor(0,0,0,1);      // cores da janela
+   glColor3f(1,0,0);           // cor da janela
+}
+void Draw()
+{
+  glClear(GL_COLOR_BUFFER_BIT);// limpando o buffer do background
+  glBegin(GL_POLYGON);          // desenhando points
+    glVertex2d(-0.5, 0.5);     // coordenadas cartesianas
+    glVertex2d( 0.5, 0.5);
+    glVertex2d( 0.5,-0.5);
+    glVertex2d(-0.5,-0.5);
+  glEnd();
+  glFlush();                   // chama as func callbacks
+}
+
+int main(int C, char *V[])
+{
+   glutInit(&C,V);
+   glutInitWindowPosition(250,50);
+   glutInitWindowSize(500,500);
+   glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+   glutCreateWindow("BASIC OPENGL");
+
+   MyInit();
+   glutDisplayFunc(Draw);
+   glutMainLoop();
+   return 0;
+}
+
+
